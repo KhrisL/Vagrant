fb4eb447f08bd529f42bb2415ce939782ef05fc0
KLeong <ka_kit1993@hotmail.co.uk>
2018-11-02 10:21:02 +0800

    Change provision type to ansible

--- centos/scripts/setup-centos-gui.sh
+++ centos/scripts/setup-centos-gui.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Setup gui mode for centos vm
 
-echo "Installing gui desktop packages"
-yum groupinstall -y 'gnome desktop'
-yum install -y 'xorg*'
-yum remove -y initial-setup initial-setup-gui
+#echo "Installing gui desktop packages"
+#yum groupinstall -y 'gnome desktop'
+#yum install -y 'xorg*'
+#yum remove -y initial-setup initial-setup-gui
 
 echo "Switch to the gui target"
 systemctl isolate graphical.target
